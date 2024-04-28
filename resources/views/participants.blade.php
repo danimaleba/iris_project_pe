2d2ffb76eaebb239635748abf08634563112808a
Daniel Levino <daniellevino@gmail.com>
2024-04-27 22:19:00 -0300

    adjustments

--- resources/views/participants.blade.php
+++ resources/views/participants.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.cleanLayout')
 @section('title', 'Participantes')
 @section('content')
+<div class="d-block">
     <h1 class="text-light">
         participantes
     </h1>
+
+    @foreach($participants as $participant)
+        <pre class="text-light">
+            {{ $participant }}
+        </pre>
+        <br/>
+    @endforeach
+</div>
+
 @endsection
